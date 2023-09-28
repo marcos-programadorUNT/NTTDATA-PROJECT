b8632414dfd10154d275af7e3196b241d43fc031
unknown <mhernandezb@unitru.edu.pe>
2023-09-28 14:53:06 -0500

    Marcos Hernandez 28/09/2023 14:53

---
+++ BankGateway/src/test/java/com/example/bankgateway/BankGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
