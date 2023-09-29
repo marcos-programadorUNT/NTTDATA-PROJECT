36a29d1cd9738fd7c07d83c253efc9664211c9f2
unknown <mhernandezb@unitru.edu.pe>
2023-09-28 19:43:54 -0500

    Marcos Hernández 28/09/2023 19:43

---
+++ MovementClient/src/main/java/com/example/movementclient/domain/model/CustomerType.java
@@ -0,0 +1,3 @@
+package com.example.movementclient.domain.model;
+
+public enum CustomerType {personal,business}
---
+++ MovementClient/src/main/java/com/example/movementclient/domain/model/PassiveBankingProductType.java
@@ -0,0 +1,5 @@
+package com.example.movementclient.domain.model;
+
+public enum PassiveBankingProductType {
+    current_account,savings_account,fixed_term_account
+}
