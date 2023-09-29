cf55050e4c035885dc71090112cd6bae58839a6c
unknown <mhernandezb@unitru.edu.pe>
2023-09-28 20:15:58 -0500

    Marcos Hernández 28/09/2023 20:15

---
+++ MovementClient/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO customers (email, address, phone_number, status, customer_type, first_name, last_name, document_number, date_of_birth)
+VALUES ('test@email.com', 'Test Address', '123456789', 'ACTIVE', 'personal', 'John', 'Doe', '12345678', '1990-01-01');
+
+INSERT INTO customers (email, address, phone_number, status, customer_type, first_name, last_name, document_number, date_of_birth)
+VALUES ('test2@email.com', 'Test Address', '123213213', 'ACTIVE', 'personal', 'Anthony', 'Kiedis', '12312312', '2001-01-01');
+
+INSERT INTO customers (email, address, phone_number, status, customer_type, business_name, ruc, foundation_date)
+VALUES ('test3@email.com', 'Test Address', '987654321', 'ACTIVE', 'business', 'Interbank', '12345678911', '1990-01-01');
+
+INSERT INTO customers (email, address, phone_number, status, customer_type, business_name, ruc, foundation_date)
+VALUES ('test4@email.com', 'Test Address', '977654321', 'ACTIVE', 'business', 'BanBif', '12312312311', '2010-01-01');
