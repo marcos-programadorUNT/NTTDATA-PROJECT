d2dfae463888bcd4c0505859ebc3c1b331dd3472
unknown <mhernandezb@unitru.edu.pe>
2023-10-01 12:38:55 -0500

    Marcos 30/09/2023 12.38

--- BankingProductClient/src/test/java/com/example/bankingproductclient/BankingProductClientApplicationTests.java
+++ BankingProductClient/src/test/java/com/example/bankingproductclient/BankingProductClientApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.bankingproductclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootTest
+@EnableEurekaClient
 class BankingProductClientApplicationTests {
 
     @Test
