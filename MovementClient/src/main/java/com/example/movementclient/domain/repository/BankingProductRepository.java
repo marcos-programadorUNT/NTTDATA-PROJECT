3246ced7f544e68f7ba8d1b9017cf60e08ec6bf8
Katoinga <evaldiviaci@unsa.edu.pe>
2023-10-01 13:15:27 -0500

    migrate to H2

--- MovementClient/src/main/java/com/example/movementclient/domain/repository/BankingProductRepository.java
+++ MovementClient/src/main/java/com/example/movementclient/domain/repository/BankingProductRepository.java
@@ -2,7 +2,7 @@ package com.example.movementclient.domain.repository;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name="banking_products")
+//@FeignClient(name="banking_products")
 public interface BankingProductRepository {
 
 }
